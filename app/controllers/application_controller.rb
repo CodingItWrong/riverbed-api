dc42ee82da97e9da46cea65d7ec79e4eb5556c45
Josh Justice <me@codingitwrong.com>
2022-12-26 07:24:18 -0500

    Expose Doorkeeper user to JR

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
-class ApplicationController < ActionController::API
+# frozen_string_literal: true
+class ApplicationController < ActionController::Base
+  include JSONAPI::ActsAsResourceController
+  skip_before_action :verify_authenticity_token
+
+  private
+
+  def context
+    { current_user: current_user }
+  end
+
+  def current_user
+    @current_user ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
 end
