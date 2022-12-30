176c0cab6b2665b8c24f18aaa165cd4bb005fa2d
Josh Justice <me@codingitwrong.com>
2022-12-30 06:44:29 -0500

    Seed cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,13 @@ User.create!(email: "example@example.com", password: "password")
 
 Field.create!(name: "title")
 Field.create!(name: "publisher")
+
+Card.create!(field_values: {
+  "title" => "Final Fantasy 7",
+  "publisher" => "Square Enix"
+})
+
+Card.create!(field_values: {
+  "title" => "Castlevania: Symphony of the Night",
+  "publisher" => "Konami"
+})
