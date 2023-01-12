9b722d3cb134900f34e90d5f258c048ee25500f8
Josh Justice <me@codingitwrong.com>
2023-01-12 08:23:41 -0500

    Add create and delete

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   use_doorkeeper
 
-  jsonapi_resources :cards, only: %w[index update]
+  jsonapi_resources :cards, only: %w[index create update destroy]
   jsonapi_resources :fields, only: %w[index]
   jsonapi_resources :users, only: %w[create]
 end
