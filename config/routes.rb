dab15a07185068325b85b1142d1d345352860f18
Josh Justice <me@codingitwrong.com>
2023-01-22 15:36:01 -0500

    Allow modifying columns

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   use_doorkeeper
 
   jsonapi_resources :cards, only: %w[index create update destroy]
-  jsonapi_resources :columns, only: %w[index]
+  jsonapi_resources :columns, only: %w[index create update destroy]
   jsonapi_resources :elements, only: %w[index create update destroy]
   jsonapi_resources :users, only: %w[create]
 end
