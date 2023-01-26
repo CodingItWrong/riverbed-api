5419f46b93bee7fed64ee19452655d9860ff8af3
Josh Justice <me@codingitwrong.com>
2023-01-26 08:49:12 -0500

    Rename sort order field to test bug

--- app/resources/column_resource.rb
+++ app/resources/column_resource.rb
@@ -1,5 +1,6 @@
 class ColumnResource < ApplicationResource
-  attributes :name, :card_inclusion_condition, :sort_order
+  attributes :name, :card_inclusion_condition
+  attribute :floof, delegate: :sort_order
 
   relationship :board, to: :one
 end
