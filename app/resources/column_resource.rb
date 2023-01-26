3fea054cd4cdac615000179c44723ea5c8a1d932
Josh Justice <me@codingitwrong.com>
2023-01-26 15:40:18 -0500

    Rename sort column to a better long-term one

--- app/resources/column_resource.rb
+++ app/resources/column_resource.rb
@@ -1,6 +1,8 @@
 class ColumnResource < ApplicationResource
   attributes :name, :card_inclusion_condition
-  attribute :floof, delegate: :sort_order
+
+  # renamed to avoid issue where JR would silently discard writes
+  attribute :card_sort_order, delegate: :sort_order
 
   relationship :board, to: :one
 end
