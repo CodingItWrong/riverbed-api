96e4e56a7a9e3bf1946120a9c633a18de8cb0809
Josh Justice <me@codingitwrong.com>
2023-03-25 10:33:05 -0400

    Test creating a board

--- spec/requests/boards_spec.rb
+++ spec/requests/boards_spec.rb
@@ -58,4 +58,47 @@ RSpec.describe "boards" do
       end
     end
   end
+
+  describe "POST /boards" do
+    let(:params) {
+      {
+        data: {
+          type: "boards",
+          attributes: {}
+        }
+      }
+    }
+
+    context "when logged out" do
+      it "returns an auth error" do
+        expect {
+          post "/boards", params: params.to_json, headers: headers
+        }.not_to change { Board.count }
+
+        expect(response.status).to eq(401)
+        expect(response.body).to be_empty
+      end
+    end
+
+    context "when logged in" do
+      include_context "with a logged in user"
+
+      it "creates and returns a board" do
+        expect {
+          post "/boards", params: params.to_json, headers: headers
+        }.to change { Board.count }.by(1)
+
+        board = Board.last
+
+        expect(response.status).to eq(201)
+
+        response_body = JSON.parse(response.body)
+        expect(response_body["data"]).to include({
+          "type" => "boards",
+          "id" => board.id.to_s,
+          "attributes" => a_hash_including("name" => nil)
+        })
+      end
+    end
+  end
 end
