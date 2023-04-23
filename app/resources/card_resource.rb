f3ec35d57259ebc87b6477fff4fff255ab0f8465
Josh Justice <me@codingitwrong.com>
2023-04-23 06:04:56 -0400

    Call webhook when updating card when configured

--- app/resources/card_resource.rb
+++ app/resources/card_resource.rb
@@ -7,9 +7,39 @@ class CardResource < ApplicationResource
     _model.user = current_user
   end
 
+  after_update do
+    update_card_from_webhook
+  end
+
   def self.records(options = {}) = current_user(options).cards
 
   def self.creatable_fields(_context) = super - [:user]
 
   def self.updatable_fields(_context) = super - [:user, :board]
+
+  private
+
+  def update_card_from_webhook
+    url = _model.board.board_options.dig("webhooks", "card-update")
+    return if url.blank?
+
+    body = {
+      "field-values" => _model.field_values,
+      "elements" => _model.board.elements.map { |elem|
+        {
+          "id" => elem.id.to_s,
+          "attributes" => {"name" => elem.name} # TODO: all attributes
+        }
+      }
+    }
+
+    response = HTTParty.patch(
+      "#{url}/#{_model.id}",
+      headers: {"Content-Type": "application/json"},
+      body: body.to_json
+    )
+    field_values_to_update = JSON.parse(response.body)
+
+    _model.update!(field_values: _model.field_values.merge(field_values_to_update))
+  end
 end
