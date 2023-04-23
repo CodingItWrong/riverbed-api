4344a0317aaa2f826e1ff282496f799afd257d05
Josh Justice <me@codingitwrong.com>
2023-04-23 06:53:26 -0400

    Hack removal of looking up title

--- app/jobs/parse_link_job.rb
+++ app/jobs/parse_link_job.rb
@@ -1,36 +1,20 @@
 # frozen_string_literal: true
 
-require "link_parser"
-
 class ParseLinkJob < ApplicationJob
   def self.parse(link_params)
     perform_later(link_params)
   end
 
   def perform(link_params)
-    parsed_link = link_parser.process(
-      url: link_params[:url],
-      timeout_seconds: 30
-    )
-
     attributes = {
-      url: parsed_link.canonical,
+      url: link_params[:url],
       title: link_params[:title]
     }
-    attributes[:title] = parsed_link.title if default_title?(link_params)
 
     save_link(attributes)
   end
 
   private
-
-  def link_parser
-    LinkParser
-  end
-
-  def default_title?(link_params)
-    link_params[:title].blank? || link_params[:title] == link_params[:url]
-  end
 
   def save_link(attributes)
     # Once this is extracted from the core API this will be a POST instead of a DB creation
@@ -38,9 +22,7 @@ class ParseLinkJob < ApplicationJob
       :user => board.user, # TODO: look up board by hard-coded ID so doesn't conflict with someone else's
       "field_values" => {
         url_field.id => attributes[:url],
-        title_field.id => attributes[:title],
-        saved_at_field.id => Time.zone.now.iso8601,
-        read_status_changed_at_field.id => Time.zone.now.iso8601
+        title_field.id => attributes[:title]
       }
     )
   end
@@ -52,8 +34,4 @@ class ParseLinkJob < ApplicationJob
   def url_field = field_by_name("URL")
 
   def title_field = field_by_name("Title")
-
-  def saved_at_field = field_by_name("Saved At")
-
-  def read_status_changed_at_field = field_by_name("Read Status Changed At")
 end
