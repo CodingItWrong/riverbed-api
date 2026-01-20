cc50667b2e87ae884c132bfbbba143ae62260f6b
Josh Justice <me@codingitwrong.com>
2026-01-20 08:14:04 -0500

    Extract shared helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,10 +65,6 @@ class UsersController < JsonapiController
     @user = User.where(id: current_user&.id).find_by(id: params[:id])
   end
 
-  def jsonapi_content_type
-    "application/vnd.api+json"
-  end
-
   def serialize_user(user)
     {
       type: "users",
@@ -78,44 +74,5 @@ class UsersController < JsonapiController
         "ios-share-board-id" => user.ios_share_board_id
       }
     }
-  end
-
-  def validate_jsonapi_request(expected_type, require_id: false, expected_id: nil)
-    begin
-      body = JSON.parse(request.body.read)
-    rescue JSON::ParserError
-      render json: {errors: [{code: "400", title: "Invalid JSON"}]}, status: :bad_request, content_type: jsonapi_content_type
-      return :error
-    end
-
-    unless body.is_a?(Hash) && body.key?("data")
-      render json: {errors: [{code: "400", title: "Missing data key"}]}, status: :bad_request, content_type: jsonapi_content_type
-      return :error
-    end
-
-    data = body["data"]
-
-    unless data.is_a?(Hash) && data["type"] == expected_type
-      render json: {errors: [{code: "400", title: "Invalid or missing type"}]}, status: :bad_request, content_type: jsonapi_content_type
-      return :error
-    end
-
-    if require_id && data["id"] != expected_id
-      render json: {errors: [{code: "400", title: "ID mismatch"}]}, status: :bad_request, content_type: jsonapi_content_type
-      return :error
-    end
-
-    {attributes: data["attributes"] || {}}
-  end
-
-  def render_not_found
-    render json: {errors: [{code: "404", title: "Record not found"}]}, status: :not_found, content_type: jsonapi_content_type
-  end
-
-  def render_validation_errors(record)
-    errors = record.errors.map do |error|
-      {code: "422", title: error.full_message}
-    end
-    render json: {errors: errors}, status: :unprocessable_entity, content_type: jsonapi_content_type
   end
 end
