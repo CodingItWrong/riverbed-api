92841d6db9424fc0cb2f1e4dcf41d5c9b3516212
Josh Justice <me@codingitwrong.com>
2026-01-20 08:07:45 -0500

    Migrate users controller off JR

--- app/resources/user_resource.rb
+++
@@ -1,9 +0,0 @@
-class UserResource < ApplicationResource
-  attributes :email, :password, :allow_emails, :ios_share_board_id
-
-  def fetchable_fields = super - [:email, :password]
-
-  def self.records(options = {})
-    User.where(id: current_user(options)&.id)
-  end
-end
