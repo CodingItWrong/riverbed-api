f573a3e471c09b5d80b8ded21505c2b4fc91e603
Josh Justice <me@codingitwrong.com>
2026-01-20 07:27:31 -0500

    Remove unnecessary content in resource

--- app/resources/board_resource.rb
+++ app/resources/board_resource.rb
@@ -1,55 +1,13 @@
+# frozen_string_literal: true
+
+# Minimal resource to support nested routes for cards, columns, and elements.
+# Board CRUD operations are handled directly by BoardsController.
 class BoardResource < ApplicationResource
-  attributes :name, :icon, :color_theme, :favorited_at
-
-  attribute :icon_extended, delegate: :icon
-  attribute :options, delegate: :board_options
-
-  # supported in original iOS app
-  ORIGINAL_ICONS = %w[
-    baseball
-    bed
-    book
-    chart
-    checkbox
-    food
-    gamepad
-    link
-    map-marker
-    medical-bag
-    money
-    scale
-    television
-    tree
-  ]
-
-  def icon
-    if ORIGINAL_ICONS.include?(_model.icon)
-      _model.icon
-    end
-  end
-
+  # Relationships needed for nested routes like /boards/:board_id/cards
   relationship :cards, to: :many
   relationship :columns, to: :many
   relationship :elements, to: :many
 
-  before_create do
-    _model.user = current_user
-  end
-
-  after_create do
-    create_initial_board_data
-  end
-
+  # Scope boards to current user for authorization on nested routes
   def self.records(options = {}) = current_user(options).boards
-
-  def self.creatable_fields(_context) = super - [:user]
-
-  def self.updatable_fields(_context) = super - [:user]
-
-  private
-
-  def create_initial_board_data
-    _model.columns.create!(user: current_user, name: "All Cards")
-    _model.cards.create!(user: current_user)
-  end
 end
