6481f2cab68023be91408bc5ed45562518f28411
Josh Justice <me@codingitwrong.com>
2026-01-21 07:22:43 -0500

    Migrate cards

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
 Rails.application.routes.draw do
   use_doorkeeper
 
-  resources :boards, only: %w[index show create update destroy] do
+  resources :boards do
     resources :columns, only: %w[index]
     resources :elements, only: %w[index]
+    resources :cards, only: %w[index]
   end
   resources :columns, only: %w[show create update destroy]
   resources :elements, only: %w[show create update destroy]
+  resources :cards, only: %w[show create update destroy]
   resources :users, only: %w[create show update destroy]
-
-  # Nested board resources still use jsonapi_resources for now
-  jsonapi_resources :boards, only: [] do
-    jsonapi_related_resources :cards
-  end
-
-  jsonapi_resources :cards, only: %w[show create update destroy]
 
   # iOS share integration
   resources :shares, only: %w[create]
